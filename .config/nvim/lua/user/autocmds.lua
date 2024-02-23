771d5e2203ae8fcaef76cb5d2a571de6fb5f7b66
avoidaccount <major0.services@protonmail>
2024-02-23 16:51:33 -0300

    stylua format

--- .config/nvim/lua/user/autocmds.lua
+++ .config/nvim/lua/user/autocmds.lua
@@ -1,4 +1,6 @@
-local function augroup(name) return vim.api.nvim_create_augroup("lazynvim_" .. name, { clear = true }) end
+local function augroup(name)
+	return vim.api.nvim_create_augroup("lazynvim_" .. name, { clear = true })
+end
 
 local autocmd = vim.api.nvim_create_autocmd
 
@@ -15,7 +17,9 @@ autocmd({
 -- highlight on yank
 autocmd("textyankpost", {
 	group = augroup("highlight_yank"),
-	callback = function() vim.highlight.on_yank() end,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- resize splits if window got resized
@@ -34,10 +38,14 @@ autocmd("bufreadpost", {
 	callback = function(event)
 		local exclude = { "gitcommit" }
 		local buf = event.buf
-		if vim.tbl_contains(exclude, vim.bo[buf].filetype) then return end
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) then
+			return
+		end
 		local mark = vim.api.nvim_buf_get_mark(buf, '"')
 		local lcount = vim.api.nvim_buf_line_count(buf)
-		if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
 	end,
 })
 
