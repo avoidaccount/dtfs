3c2bb2fa1d350704cfdf275faf98e5821e9b1763
avoidaccount <major0.services@protonmail>
2024-02-25 23:47:52 -0300

    fixed a LOT of small inconsistencies regarding $ENV_VARIABLES. added the new packages (formatters and linters) moved npm installs AFTER pulling the $XDG_CONFIG_HOME dots and source .zshrc to avoid problems on the install

--- .config/.utils/bootstrap-system.sh
+++ .config/.utils/bootstrap-system.sh
@@ -6,33 +6,31 @@ pkg upgrade -y
 
 pkg install zsh neovim rclone git nodejs python python-pip
 
-if [[ -d "$HOME/.config" ]]; then
+if [[ -d "$HOME"/.config ]]; then
 	echo "XDG_CONFIG_HOME already exists"
 else
-	mkdir -v "$HOME/.config"
+	mkdir -v "$HOME"/.config
 fi
 
-mkdir "$HOME/.shplugins"
+mkdir "$HOME"/.shplugins
 
 git clone "https://github.com/zdharma-continuum/fast-syntax-highlighting" &&
 	mv fast-syntax-highlighting "$HOME/.shplugins"
 
 git clone "git clone https://github.com/Aloxaf/fzf-tab" &&
-	mv fzf-tab "$HOME/.shplugins"
+	mv fzf-tab "$HOME"/.shplugins
 
 git clond "https://github.com/hlissner/zsh-autopair" &&
-	mv zsh-autopair "$HOME/.shplugins"
+	mv zsh-autopair "$HOME"/.shplugins
 
 apt purge nano &&
 	pkg upgrade -y
 
 pkg install 7zip aria2 bat binutils busybox difftastic dust exiftool eza fastfetch fclones fd fdupes ffmpeg flac fzf gh gitui glow golang gopass jql man maxcso navi nerdfix pkgtop procs ripgrep ripgrep-all rnr rust sd sox sqlite starship stylua tealdeer termux-api topgrade w3m w3m-img wget which zoxide
 
-pip install --no-input beautifulsoup4 gallery-dl proselint pyacoustid pylast pyyaml qobuz-dl requests yt-dlp
+pip install --no-input beautifulsoup4 gallery-dl proselint pyacoustid pylast pyright pyyaml qobuz-dl requests yamlfix yamllint yt-dlp
 
-cargo install cargo-update typos-cli
-
-mv "$HOME/.local/share/cargo/bin/*" "$PATH"
+cargo install cargo-update shellharden stylua taplo-cli typos-cli
 
 git clone "https://github.com/avoidaccount/dtfs"
 
@@ -41,6 +39,14 @@ mv "$HOME"/dtfs/.zshrc "$HOME" &&
 	mv "$HOME"/dtfs/.editorconfig "$HOME"
 
 rm -rf "$HOME"/dtfs
+
+mkdir ~/.cache/zsh
+
+compinit -d ~/.cache/zsh/zcompdump-"$ZSH_VERSION"
+
+source "$HOME"/.zshrc
+
+npm install jsonlint markdownlint-cli prettier vscode-json-languageservice write-good yaml-language-server
 
 cd "$HOME" &&
 	cd ../usr/etc &&
@@ -54,10 +60,6 @@ termux-reload-settings
 fast-theme XDG:catppuccin-mocha
 
 topgrade
-
-mkdir ~/.cache/zsh
-
-compinit -d "~/.cache/zsh/zcompdump-$ZSH_VERSION"
 
 cd && clear
 
