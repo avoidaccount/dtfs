bffdea79261ea298c1694c81bad16828b5fe0e2e
avoidaccount <major0.services@protonmail>
2024-02-27 09:52:13 -0300

    removed the format on save function

--- .config/nvim/lua/lsp/none-ls.lua
+++ .config/nvim/lua/lsp/none-ls.lua
@@ -1,5 +1,4 @@
 local none = require("null-ls")
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 none.setup({
     sources = {
@@ -60,17 +59,4 @@ none.setup({
         none.builtins.hover.dictionary,
         none.builtins.hover.printenv,
     },
-
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ async = false })
-                end,
-            })
-        end
-    end,
 })
