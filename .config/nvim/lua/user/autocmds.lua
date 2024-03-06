d9a6868f34044293b2e2a3c200b818046a27d796
avoidaccount <major0.services@protonmail>
2024-03-06 07:56:56 -0300

    removed not very useful autocmds

--- .config/nvim/lua/user/autocmds.lua
+++ .config/nvim/lua/user/autocmds.lua
@@ -4,16 +4,6 @@ end
 
 local autocmd = vim.api.nvim_create_autocmd
 
--- check if we need to reload the file when it changed
-autocmd({
-    "focusgained",
-    "termclose",
-    "termleave",
-}, {
-    group = augroup("checktime"),
-    command = "checktime",
-})
-
 -- highlight on yank
 autocmd("textyankpost", {
     group = augroup("highlight_yank"),
@@ -29,23 +19,6 @@ autocmd({ "vimresized" }, {
         local current_tab = vim.fn.tabpagenr()
         vim.cmd("tabdo wincmd =")
         vim.cmd("tabnext " .. current_tab)
-    end,
-})
-
--- go to last loc when opening a buffer
-autocmd("bufreadpost", {
-    group = augroup("last_loc"),
-    callback = function(event)
-        local exclude = { "gitcommit" }
-        local buf = event.buf
-        if vim.tbl_contains(exclude, vim.bo[buf].filetype) then
-            return
-        end
-        local mark = vim.api.nvim_buf_get_mark(buf, '"')
-        local lcount = vim.api.nvim_buf_line_count(buf)
-        if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
     end,
 })
 
